f2dba92901e27a45b7d4754529d98a2ef9801150
Youngjin Jo <yjinjo@berkeley.edu>
2023-04-19 08:05:47 +0900

    #3. 로그인

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
-from accounts.forms import SignupForm
+from accounts.forms import SignupForm, LoginForm
 from accounts.models import User
 
 signup = CreateView.as_view(
@@ -12,9 +13,10 @@ signup = CreateView.as_view(
     success_url=reverse_lazy("login"),
 )
 
-
-def login(request):
-    pass
+login = LoginView.as_view(
+    form_class=LoginForm,
+    template_name="accounts/login_form.html",
+)
 
 
 def logout(request):
