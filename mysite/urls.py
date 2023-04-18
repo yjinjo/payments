76ae0acce5a2f4b0797d113ef9fad28c334cee81
Youngjin Jo <yjinjo@berkeley.edu>
2023-04-19 08:46:07 +0900

    #4. MEDIA 설정 코드 오류 수정

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.static import static
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
