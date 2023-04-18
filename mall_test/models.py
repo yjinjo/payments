e78d0987f29507b3aba3d57e0632a910fd56266c
Youngjin Jo <yjinjo@berkeley.edu>
2023-04-18 20:43:59 +0900

    #2. 포트원 결제내역 검증 및 payment_detail 뷰를 통한 결제내역 조회

--- mall_test/models.py
+++ mall_test/models.py
@@ -1,15 +1,21 @@
+import logging
 from uuid import uuid4
 
+from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.db import models
+from django.http import Http404
+from iamport import Iamport
+
+logger = logging.getLogger("portone")
 
 
 class Payment(models.Model):
     class StatusChoices(models.TextChoices):
         READY = "ready", "미결제"
-        PAID = "paid", "결제 완료"
-        CANCELLED = "cancelled", "결제 취소"
-        FAILED = "failed", "결제 실패"
+        PAID = "paid", "결제완료"
+        CANCELLED = "cancelled", "결제취소"
+        FAILED = "failed", "결제실패"
 
     uid = models.UUIDField(default=uuid4, editable=False)
     name = models.CharField(max_length=100)
@@ -18,7 +24,6 @@ class Payment(models.Model):
             MinValueValidator(1, message="1원 이상의 금액을 지정해주세요."),
         ]
     )
-    # Portone => ready(미결제), paid(결제완료), cancelled(결제취소), failed(결제실패)
     status = models.CharField(
         max_length=9,
         default=StatusChoices.READY,
@@ -31,4 +36,20 @@ class Payment(models.Model):
     def merchant_uid(self) -> str:
         return self.uid.hex
 
-    # TODO: 포트원 REST API를 통해서 결제를 검증해야 함
+    # 포트원 REST API를 통해서 결제를 검증해야만 함.
+    def portone_check(self, commit=True):
+        api = Iamport(
+            imp_key=settings.PORTONE_API_KEY, imp_secret=settings.PORTONE_API_SECRET
+        )
+
+        try:
+            meta = api.find(merchant_uid=self.merchant_uid)
+        except (Iamport.ResponseError, Iamport.HttpError) as e:
+            logger.error(str(e), exc_info=e)
+            raise Http404(str(e))
+
+        self.status = meta["status"]
+        self.is_paid_ok = meta["status"] == "paid" and meta["amount"] == self.amount
+
+        if commit:
+            self.save()
--- mysite/settings.py
+++ mysite/settings.py
@@ -74,7 +74,6 @@ ROOT_URLCONF = "mysite.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        # "DIRS": [],
         "DIRS": [
             BASE_DIR / "templates",
         ],
@@ -126,7 +125,6 @@ AUTH_USER_MODEL = "accounts.User"
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-# LANGUAGE_CODE = "en-us"
 LANGUAGE_CODE = env.str("LANGUAGE_CODE", default="ko-kr")
 
 TIME_ZONE = "Asia/Seoul"
@@ -139,7 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = env.str("STATIC_ROOT", default=BASE_DIR / "staticfiles")
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = env.str("MEDIA_ROOT", default=BASE_DIR / "mediafiles")
