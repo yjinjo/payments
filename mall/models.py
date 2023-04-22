98456e6281d567fa7a07abe7c539003c8826711d
Youngjin Jo <yjinjo@berkeley.edu>
2023-04-22 10:46:43 +0900

    #5. 주문 모델 설계

--- mall/models.py
+++ mall/models.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 from django.core.validators import MinValueValidator
 from django.db import models
 from django.db.models import UniqueConstraint
@@ -79,3 +81,50 @@ class CartProduct(models.Model):
                 name="unique_user_product",
             )
         ]
+
+
+class Order(models.Model):
+    class Status(models.TextChoices):
+        REQUESTED = "requested", "주문 요청"
+        FAILED_PAYMENT = "failed_payment", "결제 실패"
+        PAID = "paid", "결제 완료"
+        PREPARED_PRODUCT = "prepared_product", "상품 준비중"
+        SHIPPED = "shipped", "배송중"
+        DELIVERED = "delivered", "배송 완료"
+        CANCELED = "canceled", "주문 취소"
+
+    uid = models.UUIDField(default=uuid4, editable=False)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        db_constraint=False,
+    )
+    total_amount = models.PositiveIntegerField("결제 금액")
+    status = models.CharField(
+        "진행 상태",
+        max_length=20,
+        choices=Status.choices,
+        default=Status.REQUESTED,
+        db_index=True,
+    )
+    product_set = models.ManyToManyField(
+        Product,
+        through="OrderedProduct",
+        blank=False,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class OrderedProduct(models.Model):
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        db_constraint=False,
+    )
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, db_constraint=False)
+    name = models.CharField("상품명", max_length=100, help_text="주문 시점의 상품 명을 저장합니다.")
+    price = models.PositiveIntegerField("상품 가격", help_text="주문 시점의 상품 가격을 저장합니다.")
+    quantity = models.PositiveIntegerField("수량")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
